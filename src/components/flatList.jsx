fcc35af4c016bc87ebe774bcc2feae353c273065
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-19 22:35:21 +0100

    fill flat card with flat details : picture, price and description

--- src/components/flatList.jsx
+++ src/components/flatList.jsx
@@ -8,8 +8,15 @@ class FlatList extends Component {
     }
 
     renderlist (flat) {
-        return(
-            <div className="card">{flat.id}</div>
+        const urlBackground = flat.imageUrl;
+        return(           
+            <div className="card" style={{backgroundImage: `url(${urlBackground}) , linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2))`}}>                
+                <div className="card-category">{flat.price} {flat.priceCurrency}</div>
+                <div className="card-description">
+                    <h2>{flat.name}</h2>
+                </div>
+                <a className="card-link" href="#"></a>
+            </div>
         )
     }
 
