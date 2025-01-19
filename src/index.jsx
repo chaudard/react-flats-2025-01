8d78f344c1dcd635cce05acec5bfd3100c63ae81
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-19 15:55:04 +0100

    change main id name to match with css

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from "./components/app";
 import '../assets/stylesheets/application.scss';
 
-const container = document.getElementById('app');
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App /> );
