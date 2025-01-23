f098c0558c523593f6694ff2adfd78cc6dd004ab
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-24 00:48:58 +0100

    compute className to highlight selected flat

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -10,10 +10,19 @@ class Flat extends Component {
         this.props.handleClick(this.props.flat.id)
     }
 
+    computeClassName = () => {
+        let result = "card";
+        if (this.props.flat.id == this.props.selectedFlatId) {
+            result += " active";
+        }
+        return result
+    }
+
     render (){
         const urlBackground = this.props.flat.imageUrl;
+        const computedClassName = this.computeClassName();
         return(           
-            <div className="card" onClick={this.handleClick} style={{backgroundImage: `url(${urlBackground}) , linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2))`}}>                
+            <div className={computedClassName} onClick={this.handleClick} style={{backgroundImage: `url(${urlBackground}) , linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2))`}}>                
                 <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
                 <div className="card-description">
                     <h2>{this.props.flat.name}</h2>
