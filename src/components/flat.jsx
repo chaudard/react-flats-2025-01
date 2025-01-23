b435d041cd2a972de1cea4207ad71d613abfed8b
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-23 11:03:57 +0100

    create and use Flat component

---
+++ src/components/flat.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+
+class Flat extends Component {
+    constructor (props){
+        super(props);
+    }
+
+    render (){
+        const urlBackground = this.props.flat.imageUrl;
+        return(           
+            <div className="card" style={{backgroundImage: `url(${urlBackground}) , linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2))`}}>                
+                <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
+                <div className="card-description">
+                    <h2>{this.props.flat.name}</h2>
+                </div>
+                <a className="card-link" href="#"></a>
+            </div>
+        )
+    }
+}
+
+export default Flat;
--- src/components/flatList.jsx
+++ src/components/flatList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import flats from "../../data/flats";
+import Flat from "./flat";
 
 class FlatList extends Component {
 
@@ -8,15 +9,8 @@ class FlatList extends Component {
     }
 
     renderlist (flat) {
-        const urlBackground = flat.imageUrl;
-        return(           
-            <div className="card" style={{backgroundImage: `url(${urlBackground}) , linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2))`}}>                
-                <div className="card-category">{flat.price} {flat.priceCurrency}</div>
-                <div className="card-description">
-                    <h2>{flat.name}</h2>
-                </div>
-                <a className="card-link" href="#"></a>
-            </div>
+        return(
+            <Flat flat={flat} key={flat.id}></Flat>
         )
     }
 
