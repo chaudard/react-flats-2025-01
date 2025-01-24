51cab2edbc358908aba90791f53108e4cab6d1f2
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-24 01:48:50 +0100

    show marker (red circle) on map when click on flat

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import Map from "./map";
 import FlatList from "./flatList";
+import flats from "../../data/flats";
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {selectedFlatId: 0};
+        this.state = {selectedFlatId: 0, showMarker: false, lat: 0, lng: 0};
     }
 
     handleClick = (flatId) => {
         if (flatId != this.state.selectedFlatId) {
-            this.setState({selectedFlatId: flatId});
+            const flat = flats[flatId-1];
+            this.setState({selectedFlatId: flatId, showMarker: true, lat: flat.lat, lng: flat.lng});
         }       
     }
 
@@ -19,7 +21,7 @@ class App extends Component {
             <div>
                 <FlatList selectedFlatId={this.state.selectedFlatId} handleClick={this.handleClick}></FlatList>            
                 <div className="map-container">
-                    <Map></Map>
+                    <Map showMarker={this.state.showMarker} lat={this.state.lat} lng={this.state.lng}></Map>
                 </div>
             </div>
         )
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import GoogleMapReact from 'google-map-react';
 
+const Marker = () => <div className="marker"></div>;
+
 class Map extends Component {
 
     constructor (props) {
@@ -8,17 +10,38 @@ class Map extends Component {
     }
 
     render () {
-        return(
+        const defaultZoom = 12;
+        if (this.props.showMarker) {
+            return(                
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
                  <GoogleMapReact
                     bootstrapURLKeys={{ key: "" }}
-                    defaultCenter={{lat: 10.99835602, lng: 77.01502627}}
-                    defaultZoom={11}
+                    defaultCenter={{lat: this.props.lat, lng: this.props.lng}}
+                    defaultZoom={defaultZoom}
                 >
+                    <Marker
+                        lat={this.props.lat}
+                        lng={this.props.lng}
+                    />
                 </GoogleMapReact>            
-            </div>        )
-    }
+            </div>        
+            )
+        }
+        else {
+            return(                
+                // Important! Always set the container height explicitly
+                <div style={{ height: '100vh', width: '100%' }}>
+                     <GoogleMapReact
+                        bootstrapURLKeys={{ key: "" }}
+                        defaultCenter={{lat: 48.884211, lng: 2.34689}}
+                        defaultZoom={defaultZoom}
+                    >
+                    </GoogleMapReact>            
+                </div>        
+                )
+            }
+}
 
 }
 
